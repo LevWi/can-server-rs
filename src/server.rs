700a56a79847b5608a1cb43addbbd70d3e012408
Lev Fomenko <flevvi@gmail.com>
2019-11-17 14:47:15 +0300

    Draft server implementation. 2

--- src/server.rs
+++ src/server.rs
@@ -2,55 +2,54 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread::{ self, JoinHandle};
 
-pub struct Server {
-    work_flag : Arc<AtomicBool>,
-    sender: Option<JoinHandle<()>>,
-    listener: Option<JoinHandle<()>>
+pub struct Builder {
+
 }
 
-impl Server {
-    pub fn new() -> Server {
-        Server { work_flag : Arc::new(AtomicBool::new(false)), sender : None, listener : None }
-    }
+pub struct Server {
+    work_flag : Arc<AtomicBool>,
+    jobs: Vec<JoinHandle<()>>,
+}
 
-    pub fn start(&mut self) {
+impl Builder {
 
-        let work_flg = self.work_flag.clone();
-        self.listener = Some( thread::Builder::new()
+    pub fn start(self) -> Server {
+
+        let flag = Arc::new(AtomicBool::new(true));
+        let work_flg = flag.clone();
+
+        let listener = thread::Builder::new()
                 .name("listener".into())
                 .spawn( move || { 
            
             while work_flg.load(Ordering::Acquire) {
-                // TODO
+                //TODO
             } 
 
-        } ).unwrap()); 
+        } ).unwrap(); 
 
-
-        let work_flg = self.work_flag.clone();
-        self.sender = Some( thread::Builder::new()
+        let work_flg = flag.clone();
+        let sender = thread::Builder::new()
                 .name("sender".into())
                 .spawn( move || { 
            
             while work_flg.load(Ordering::Acquire) {
-                // TODO
+                //TODO
             } 
 
-        } ).unwrap());
-    }
+        } ).unwrap();
 
-    pub fn stop(&mut self) {
-        self.work_flag.store(false, Ordering::Release);
-        
-        if self.sender.is_some() {
-            //error self.sender.unwrap().join();
-        }
-
+        Server { work_flag : flag, jobs : vec![listener, sender] }
     }
 }
 
-impl Drop for Server {
-    fn drop(&mut self) {
-        //TODO
+impl Server {
+    pub fn stop(self) {
+        self.work_flag.store(false, Ordering::Release);
+        
+        for j in self.jobs.into_iter() {
+            j.join().unwrap();
+        }
+
     }
 }
